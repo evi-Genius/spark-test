632885678b09d2635cb4a79fa6e55dea00e41aa4
evi-Genius <x1074972500@163.com>
2018-11-11 22:13:27 +0800

    按用户出现次数前10

--- sort.py
+++ sort.py
@@ -3,11 +3,13 @@ from pyspark import SparkContext, SparkConf
 
 # var conf = new SparkConf()
 conf = SparkConf().setAppName("My app").setMaster("local")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 # textFile = sc.textFile("file:///home/xiang/Desktop/sogou.500w.utf8")
-textFile = sc.textFile("/sparksorts")
-textFile.persist()
-print(textFile.count())
-lists=textFile.collect()#collct把RDD转成list
-for i in lists[0:30]:
+# textFile = sc.textFile("/sparksorts")
+textFile = sc.textFile("/sogou_ext/20111230")
+uIDRdd = textFile.map(lambda x: (
+    x.split("\t")[1], 1)).reduceByKey(lambda x, y: x+y)
+uIDRdd=uIDRdd.sortBy(lambda x:x[1],ascending=False)
+lists = uIDRdd.take(10)
+for i in lists:
     print(i)
