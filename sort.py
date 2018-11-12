e2a101d524c51582beb2f69711a7ea3ecd87496b
evi-Genius <x1074972500@163.com>
2018-11-12 16:34:27 +0800

    显示o2a855用户的前15搜索记录

--- sort.py
+++ sort.py
@@ -1,24 +1,16 @@
 # encoding: utf-8
 from pyspark import SparkContext, SparkConf
-from collections import defaultdict
-
-
+import json
 # var conf = new SparkConf()
 conf = SparkConf().setAppName("My app").setMaster("local")
 sc = SparkContext(conf=conf)
-
-
 # textFile = sc.textFile("file:///home/xiang/Desktop/sogou.500w.utf8")
 # textFile = sc.textFile("/sparksorts")
 textFile = sc.textFile("/sogou_ext/20111230")
-#每人搜索次数(id,time)
-pairs = textFile.map(lambda x: (
-    x.split("\t")[1], 1)).reduceByKey(lambda x, y: x+y)
-#time
-lens=pairs.values()
-#每种time的个数
-res=sorted(lens.countByValue().items())
-#返回的是dict格式
-for i in res:
-    print (i)
-# uIDRdd=uIDRdd.sortBy(lambda x:x[1],ascending=False)
+uIDRdd = textFile.filter(lambda x:x.split("\t")[1]=="02a8557754445a9b1b22a37b40d6db38")
+usearch=uIDRdd.map(lambda x:(x.split("\t")[2],1)).reduceByKey(lambda x,y:x+y)
+usearch=usearch.sortBy(lambda x:x[1],False)
+lists = usearch.take(15)
+for i in lists:
+    # print json.dumps(i,encoding='utf-8',ensure_ascii=False)
+    print(i)
